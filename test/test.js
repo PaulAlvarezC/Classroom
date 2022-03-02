a825bc2f88c50843c34c01acbf08366cf6a184be
Paul Alvarez <paul_alvarez11@outlook.com>
2022-03-01 21:51:29 -0500

    Update

--- test/test.js
+++ test/test.js
@@ -3,19 +3,19 @@ var ex = require('../solution.js');
 
 describe('Call 1st Function', function() {
 	it('Match Response Message', function() {
-	    expect(ex.first()).to.equal('Success: 1st function works!');
+	    expect(ex.first()).to.equal('Success: Hola');
 	});
 });
 
 describe('Call 2nd Function', function() {
 	it('Match Response Message', function() {
-	    expect(ex.second()).to.equal('Success: 2nd function works!');
+	    expect(ex.second()).to.equal('Success: Usuario');
 	});
 });
 
 describe('Call 3rd Function', function() {
 	it('Match Response Message', function() {
-	    expect(ex.third()).to.equal('Success: 3rd function works!');
+	    expect(ex.third()).to.equal('Success: GitHub Classroom');
 	});
 });
 
